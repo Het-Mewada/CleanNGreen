813eabc59a2efb3381279b639ab2bf0636c9d2ec
Het-Mewada <hetmwd1384@gmail.com>
2025-06-10 22:07:46 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,7 +13,7 @@ router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
 
-router.get('/google', ()=> { console.log("readched /google")},passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google',passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
   console.log("reached here")
