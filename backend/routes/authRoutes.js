49ddf5286221d76417c364096af045d81a95bf2a
Mewada Het <hetmwd1384@gmail.com>
2025-06-10 22:26:40 +0530

    refresh bug solved

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,9 +13,9 @@ router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
 
-router.get('/google', ()=> { console.log("readched /google")},passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google',passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
+router.get('/google/callback', ()=>{console.log("reached callback")} ,  passport.authenticate('google', { session: false }), (req, res) => {
   console.log("reached here")
   const user = req.user?.toObject ? req.user.toObject() : { ...req.user };
   const token = jwt.sign( user , process.env.JWT_SECRET, { expiresIn: '7d' });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,8 +10,9 @@ const backendPort = 5000;
 export default defineConfig({
   plugins: [react() , tailwindcss()],
   define: {
-    __API_URL__: JSON.stringify(`https://cleanngreen.onrender.com/api`)
+    // __API_URL__: JSON.stringify(`https://cleanngreen.onrender.com/api`)
     // __API_URL__: JSON.stringify(`http://localhost:5000/api`)
+    __API_URL__: JSON.stringify(`http://192.168.200.31:5000/api`)
   },
 })
   
