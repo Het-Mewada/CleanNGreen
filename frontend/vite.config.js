132c900c092fc1307eed0c7498183cd8f219acb5
Mewada Het <hetmwd1384@gmail.com>
2025-06-24 20:43:17 +0530

    error solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import localIpUrl from 'local-ip-url';
 
-const localIp = localIpUrl('public'); // gets your current LAN IP (e.g., 192.168.1.x)
-const backendPort = 5000;
 
 // https://vite.dev/config/
 export default defineConfig({
